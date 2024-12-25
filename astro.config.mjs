f65036c77bdb018168a2ba6c3b55f8bf9e18809c
Minoo Jang <minoojang@gmail.com>
2024-12-24 19:44:20 -0800

    tutorial components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://trominoes.github.io',
-    base: '/astro-tutorial'
+  site: 'https://trominoes.github.io',
+  base: '/astro-tutorial',
+  integrations: [preact()]
 });
